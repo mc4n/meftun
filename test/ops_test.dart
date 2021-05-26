ea6a9cf6e0225ce5b902d02774de5d63d320b1dc
Mustafa Can <mustcean@gmail.com>
2021-05-26 13:15:54 +0300

    path.join used for file path

--- test/ops_test.dart
+++ test/ops_test.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:me_flutting/helpers/filehelpers.dart';
+import 'package:path/path.dart';
 
 void main() {
   test('testFileTempDir', () async {
@@ -9,7 +9,7 @@ void main() {
       print('dir: ${temp.path} created.');
 
       //brda $temp ile biseler yap
-      var f = File('${temp.path}/new_file.txt');
+      var f = File(join(temp.path, 'new_file.txt'));
 
       f.writeAsStringSync('hello world!');
 
