cbfac449d2ee6d4e18ea9d1362f11e58b1b52faf
Mustafa Can <mustcean@gmail.com>
2021-06-03 00:53:04 +0300

    chatItem redesigned

--- lib/widgets/chatitem.dart
+++ lib/widgets/chatitem.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/rendering.dart';
+import 'package:me_flutting/models/chat.dart';
 import '../helpers/msghelper.dart' show MessageFactory;
 import '../pages/texting.dart' show TextingPage;
 
@@ -18,67 +19,86 @@ class ChatItem extends StatefulWidget {
 class ChatItemState extends State<ChatItem> {
   @override
   Widget build(BuildContext context) {
-    var lastMsg = widget.messageFactory.lastMessage;
-    if (lastMsg.body == null) return Row();
+    final lastMsg = widget.messageFactory.lastMessage;
+    final meOrCaption = (Chat _) =>
+        _ == widget.messageFactory.chatFactory.owner ? 'YOU' : _.caption;
+    final from = meOrCaption(lastMsg.from);
+    final to = meOrCaption(lastMsg.chatGroup);
+    final dt = lastMsg.epochToTimeString();
+    final fromAv = lastMsg.from.photoURL;
+    final toAv = lastMsg.chatGroup.photoURL;
+    final msgStatFrom =
+        lastMsg.epoch % 2 == 0 ? Colors.grey.shade700 : Colors.blue.shade300;
+    final msgStatTo = (lastMsg.epoch + DateTime.now().second) % 2 == 0
+        ? Colors.grey.shade700
+        : Colors.blue.shade300;
+    const PAD_AV_AR = 40.0;
 
-    Widget avatarName(String tx, String av) {
-      return Column(children: [
-        Text('$tx'),
-        Padding(padding: EdgeInsets.symmetric(vertical: 3)),
-        CircleAvatar(backgroundImage: AssetImage(av))
-      ]);
-    }
-
-    List<Widget> afterAvatar(String body) {
-      return [
-        Padding(padding: EdgeInsets.symmetric(horizontal: 11.0)),
-        Padding(padding: EdgeInsets.only(left: 15.0)),
-        Text("${body.length <= 25 ? body : body.substring(0, 25) + '...'}")
-      ];
-    }
-
-    var avatarAndText = <Widget>[];
-    avatarAndText.add(avatarName(
-        '${lastMsg.from == widget.messageFactory.chatFactory.owner ? 'YOU' : lastMsg.from.caption}',
-        lastMsg.from.photoURL));
-
-    avatarAndText.add(Text('    ->    '));
-
-    avatarAndText.add(avatarName(
-        '${lastMsg.chatGroup == widget.messageFactory.chatFactory.owner ? 'YOU' : lastMsg.chatGroup.caption}',
-        lastMsg.chatGroup.photoURL));
-
-    avatarAndText.add(Padding(padding: EdgeInsets.symmetric(horizontal: 20.0)));
-
-    avatarAndText.addAll(afterAvatar('${lastMsg.body}'));
-
-    avatarAndText.addAll([
-      Padding(padding: EdgeInsets.only(left: 15.0)),
-      Text(lastMsg.epochToTimeString(),
-          style: TextStyle(color: Colors.grey.shade700, fontSize: 12))
-    ]);
-
-    var card = Card(
-      key: ValueKey(widget.messageFactory.chatItem.id),
-      child: Padding(
-        child: GestureDetector(
-            child: Row(children: avatarAndText),
-            onLongPress: () {
-              print('onLongPress ');
-            }),
-        padding: EdgeInsets.symmetric(vertical: 10.0),
-      ),
-    );
-
-    return TextButton(
-        onPressed: () => {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) =>
-                      TextingPage(widget.messageFactory, widget.onMsgSent),
-                ),
+    return _frame(Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(children: [
+              Column(
+                children: [
+                  CircleAvatar(backgroundImage: AssetImage(fromAv)),
+                  Text(from)
+                ],
+              ),
+              Column(children: [
+                Icon(Icons.arrow_forward, color: msgStatFrom),
+                Padding(padding: EdgeInsets.symmetric(horizontal: PAD_AV_AR)),
+                Text(dt,
+                    style:
+                        TextStyle(color: Colors.grey.shade700, fontSize: 12)),
+              ]),
+            ]),
+            Container(
+              color: Colors.grey.shade100,
+              child: Text(
+                  lastMsg.body.length > 20
+                      ? '${lastMsg.body.substring(0, 20)}...'
+                      : lastMsg.body,
+                  style: TextStyle(fontSize: 14)),
+            ),
+            Row(children: [
+              Column(children: [
+                Icon(Icons.arrow_forward, color: msgStatTo),
+                Padding(padding: EdgeInsets.symmetric(horizontal: PAD_AV_AR)),
+                Text('xx:xx',
+                    style:
+                        TextStyle(color: Colors.grey.shade700, fontSize: 12)),
+              ]),
+              Column(
+                children: [
+                  CircleAvatar(backgroundImage: AssetImage(toAv)),
+                  Text(to)
+                ],
               )
-            },
-        child: card);
+            ]),
+          ],
+        )));
   }
+
+  Widget _frame(Widget _inner) => TextButton(
+      onPressed: () => {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) =>
+                    TextingPage(widget.messageFactory, widget.onMsgSent),
+              ),
+            )
+          },
+      child: Card(
+        key: ValueKey(widget.messageFactory.chatItem.id),
+        child: Padding(
+          child: GestureDetector(
+              child: _inner,
+              onLongPress: () {
+                print('onLongPress ');
+              }),
+          padding: EdgeInsets.symmetric(vertical: 10.0),
+        ),
+      ));
 }
