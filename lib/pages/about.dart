141bb4622245514fdd002c3ad0006470331696bb
Mustafa Can <mustcean@gmail.com>
2021-06-20 18:03:43 +0300

    modified:   lib/pages/about.dart

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -5,5 +5,14 @@ Widget aboutPage(BuildContext context) => TextButton(
         context: context,
         builder: (_) => Center(
             child: Material(
-                color: Colors.transparent, child: Card(child: Text('#'))))),
+                color: Colors.transparent,
+                child: Card(
+                    color: Colors.yellow.shade200,
+                    child: Padding(
+                        padding: EdgeInsets.all(30),
+                        child: Text(
+                            '''Bu uygulama Dr. Öğretim Üyesi Ahmet Cevahir ÇINAR tarafından
+yürütülen 3301456 kodlu MOBİL PROGRAMLAMA dersi kapsamında 
+193301069 numaralı Mustafa C. Akpınar tarafından 25 Haziran 2021 
+günü yapılmıştır.''')))))),
     child: Text('About'));
