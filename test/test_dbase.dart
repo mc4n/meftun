b6836f549a6a1aea239ce5a97daf96bea1ebe989
Mustafa Can <mustcean@gmail.com>
2021-07-04 14:44:10 +0300

    started redesigning tablebase functions

--- test/test_dbase.dart
+++ test/test_dbase.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:me_flutting/models/basemodel.dart';
 import 'package:me_flutting/tables/dbase_manager.dart';
-import 'package:me_flutting/tables/sembast_helper.dart';
+import 'package:me_flutting/tables/sembast_helper_v2.dart';
 
 class TestModel extends ModelBase {
   final String userName;
@@ -12,7 +12,7 @@ class TestModel extends ModelBase {
   Map<String, dynamic> get map => {'user_name': userName, 'age': age};
 }
 
-class TestTable with SembastHelper<TestModel> {
+class TestTable with SembastHelperV2<TestModel> {
   final String _name;
   final SembastDbManager _manager;
   TestTable(this._manager, [this._name = 'tbTest']);
@@ -35,9 +35,15 @@ TestTable get myTestTable => SembastDbManager(false)
     .table('tbTest', tableBuilder: (man, [name]) => TestTable(man, name));
 
 void main() {
-  test('get test table', () async {
-    final myTable = myTestTable;
-
-    expect(myTable, isNotNull);
+  test('list test table', () async {
+    final ls = await myTestTable.list(orderBy: '-user_name');
+    print(ls.map((i) => i.userName));
+    expect(ls.length, 4);
   });
+
+  /*test('insert test table', () async {
+    final ls = await myTestTable.insert(TestModel('memo', 7));
+
+    expect(ls, isNot(0));
+  });*/
 }
